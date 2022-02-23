08fabbdabae872ca7a2fc3cca956a311c8e58823
AdrianBurgosA <aaburgos1@espe.edu.ec>
2022-02-22 23:10:52 -0500

    Added new design for home page

--- 06-Code/APIS/cake-web-main/src/user/ProductsPage.js
+++ 06-Code/APIS/cake-web-main/src/user/ProductsPage.js
@@ -1,28 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import MainUserPage from '../templates/MainUserPage'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import api from '../api/Axios'
 import ErrorPage from '../public/ErrorPage'
-import Modal from '../utils/shopModal'
-import Box from '@mui/material/Box';
-import { brown } from '@mui/material/colors';
-import { styled } from '@mui/material/styles';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import SpeedDial from '@mui/material/SpeedDial';
-import ListProducts from '../utils/ListProducts';
-import Banner from '../utils/Banner';
 
 const ProductsPage = () => {
     const [ rol, setRol ] = useState('')
-    const [products, setProducts] = useState([])
-    const [selected, setSelected] = useState({})
     const [user, setUser] = useState({name: '', rol: '' ,token: '', username: ''})
-    const [ isOpen, setOpen ] = useState(false)
-    const [ isCartOpen, setCartOpen ] = useState(false)
 
     document.title = "Products"
 
@@ -44,7 +27,6 @@ const ProductsPage = () => {
                     }
                 }
                 const response = await api.get('/products',config)
-                setProducts(response.data)
             }catch(err){
                 if(err.response){
                     console.log(err.response.data)
@@ -63,7 +45,7 @@ const ProductsPage = () => {
         return(
             <MainUserPage>
                 <center>
-                <img class="logo" src="https://github.com/EzequielCastilloH/T1_C-out_E-commerce/blob/main/06-Code/E-commerce%20Code/JAVA/web/img/logo.png?raw=true" width="300" height="300"/>
+                <img class="logo" src="../img/home.jpg" width="300" height="300"/>
                 </center>
             </MainUserPage>
         )
